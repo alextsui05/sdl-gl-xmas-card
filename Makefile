f36caaa0b5d65175b4aa06af42d6d1b3242bff9f
Alex Tsui <alextsui05@gmail.com>
2016-12-25 00:55:10 -0800

    Make it a Christmas card

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all: hello_tri hello_owl.c
 
 hello_tri: hello_tri.cpp shaders.cpp
 	g++ $^ -std=c++11 -lSDL2 -lSDL2_image -lGL -lGLEW -o hello_tri
-	$(CXX) $^ -O2 -s USE_SDL=2 -o hello_tri.html
+	$(CXX) $^ -O2 -s USE_SDL=2 --preload-file shaders -o hello_tri.html
 
 .PHONY: clean
 clean:
--- shaders.h
+++ shaders.h
@@ -1,10 +1,10 @@
 #ifndef SHADERS_H
 #define SHADERS_H
 
-//#include <GLES2/gl2.h>
 #include <GL/glew.h>
 
 GLuint loadShader(GLenum type, const char *source);
-GLuint buildProgram(GLuint vertexShader, GLuint fragmentShader, const char * vertexPositionName);
+GLuint loadShaderFromFile(GLenum type, const char *filename);
+GLuint buildProgram(GLuint vertexShader, GLuint fragmentShader, const char* vertexPositionName);
 
 #endif
